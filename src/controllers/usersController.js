19532d968fa269f346e4dca5a13cc8a2e30a01dc
Eric <menaericdaniel@gmail.com>
2024-01-18 09:24:42 -0300

    feat: add session y cookies

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -61,6 +61,9 @@ module.exports = {
     logout : (req,res) => {
         
         req.session.destroy();
+        res.cookie('kitchening4EV3R_user',null,{
+            maxAge : -1
+        })
 
         return res.redirect('/')
     },
