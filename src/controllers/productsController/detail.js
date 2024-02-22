e0ead421b12e95922097bd01c5f2e71e4bfd4b4e
Eric <menaericdaniel@gmail.com>
2024-02-22 09:08:54 -0300

    add details css

--- src/controllers/productsController/detail.js
+++ src/controllers/productsController/detail.js
@@ -1,35 +1,23 @@
-const db = require('../../database/models')
+const db = require("../../database/models");
 
-module.exports = (req,res) => {
+module.exports = (req, res) => {
+  const { id } = req.params;
 
-    const {id} = req.params;
-
-    const resto = db.Restaurant.findByPk(id,{
-        include : ['images','category','address']
-    })
-
-    const bookings = db.Booking.findAll({
-        where : {
-            restaurantId : id
+  db.Restaurant.findByPk(id, {
+    include: ["images", "category", "address"],
+  })
+    .then((resto) => {
+      db.Restaurant.findAll({
+        where: {
+          categoryId: resto.categoryId,
         },
-        attributes : ['quantity']
+        include: ["images", "category", "address"],
+      }).then((relateds) => {
+        return res.render("products/product-detail", {
+          ...resto.dataValues,
+          relateds,
+        });
+      });
     })
-
-    Promise.all([resto, bookings])
-
-
-        .then(([resto, bookings]) => {
-
-            const total = bookings.map(booking => booking.quantity).reduce((acum, sum) => acum + sum, 0)
-
-            const availability = resto.capacity - total
-            
-            return res.render('products/product-detail',{
-                ...resto.dataValues,
-                availability
-            })
-        })
-        .catch(error => console.log(error))
-
-  
-}
+    .catch((error) => console.log(error));
+};
